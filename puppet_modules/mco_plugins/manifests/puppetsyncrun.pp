47b1f32bfbfd2ba87331c5590b3abfd91994555b
HongFa Tang <hongfa.tang@ericsson.com>
2015-03-17 14:57:01 +1100

    try

--- puppet_modules/mco_plugins/manifests/puppetsyncrun.pp
+++ puppet_modules/mco_plugins/manifests/puppetsyncrun.pp
@@ -1,14 +1,25 @@
 # /etc/puppetlabs/puppet/modules/mco_plugins/manifests/puppetsyncrun.pp
 class mco_plugins::puppetsyncrun {
-  Class['pe_mcollective::server::plugins'] -> Class[$title] ~> Service['pe-mcollective']
-  include pe_mcollective
-  $plugin_basedir = $pe_mcollective::server::plugins::plugin_basedir
-  $mco_etc        = $pe_mcollective::params::mco_etc
+  #Class['pe_mcollective::server::plugins'] -> Class[$title] ~> Service['pe-mcollective']
+  #include pe_mcollective
+  #$plugin_basedir = $pe_mcollective::server::plugins::plugin_basedir
+  #$mco_etc        = $pe_mcollective::params::mco_etc
+
+  #File {
+  #  owner => $pe_mcollective::params::root_owner,
+  #  group => $pe_mcollective::params::root_group,
+  #  mode  => $pe_mcollective::params::root_mode,
+  #}
+
+  Class['puppet_enterprise::server::plugins'] -> Class[$title] ~> Service['pe-mcollective']
+  include puppet_enterprise
+  $plugin_basedir = $puppet_enterprise::server::plugins::plugin_basedir
+  $mco_etc        = $puppet_enterprise::params::mco_etc
 
   File {
-    owner => $pe_mcollective::params::root_owner,
-    group => $pe_mcollective::params::root_group,
-    mode  => $pe_mcollective::params::root_mode,
+    owner => $puppet_enterprise::params::root_owner,
+    group => $puppet_enterprise::params::root_group,
+    mode  => $puppet_enterprise::params::root_mode,
   }
 
   file {"${plugin_basedir}/agent/puppetsyncrun.ddl":
