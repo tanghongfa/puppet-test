76eabe7620e90635eb2ba1dea123ce1b8231a746
HongFa <hongfa@puppetmaster.com>
2015-03-22 19:54:37 +0000

    add new dvn2 linkmanager module

---
+++ dvn2_linkmanager/manifests/init.pp
@@ -0,0 +1,41 @@
+# == Class: dvn2_linkmanager
+#
+# Full description of class dvn2_linkmanager here.
+#
+# === Parameters
+#
+# Document parameters here.
+#
+# [*sample_parameter*]
+#   Explanation of what this parameter affects and what it defaults to.
+#   e.g. "Specify one or more upstream ntp servers as an array."
+#
+# === Variables
+#
+# Here you should define a list of variables that this module would require.
+#
+# [*sample_variable*]
+#   Explanation of how this variable affects the funtion of this class and if
+#   it has a default. e.g. "The parameter enc_ntp_servers must be set by the
+#   External Node Classifier as a comma separated list of hostnames." (Note,
+#   global variables should be avoided in favor of class parameters as
+#   of Puppet 2.6.)
+#
+# === Examples
+#
+#  class { 'dvn2_linkmanager':
+#    servers => [ 'pool.ntp.org', 'ntp.local.company.com' ],
+#  }
+#
+# === Authors
+#
+# Author Name <author@domain.com>
+#
+# === Copyright
+#
+# Copyright 2015 Your name here, unless otherwise noted.
+#
+class dvn2_linkmanager {
+
+
+}
